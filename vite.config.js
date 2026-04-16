07a0dfd1f8fadfaee0bb8761aebe6699f16b405f
reeshaa <shenoyreesha@gmail.com>
2026-04-16 23:03:48 +0530

    fix: update base path in Vite configuration for correct deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/asset-management-survey/',
+  base: '/',
   plugins: [react()],
 })
